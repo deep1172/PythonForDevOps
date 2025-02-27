c7416624feac57aaae49f85d2ce72522d1d16b36
Deepak Kumar <110776915+deep1172@users.noreply.github.com>
2025-02-27 12:39:31 +0000

    add requirements file

--- Basics/hello.py
+++ Basics/hello.py
@@ -1,1 +1,13 @@
 print('Hello')
+print(1+2)
+variable = 34
+print(type(variable))
+
+variable = "Rajan"
+print(type(variable))
+
+variable = "34"
+print(type(variable))
+
+age = int(input("What is your age "))
+print(age)
